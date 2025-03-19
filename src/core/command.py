5ffd38e7c1e57d61bf4bdd8d59b5fab3993fc3c0
Floating-Ocean <sea113290980@qq.com>
2025-03-19 23:43:52 +0800

    docs(Core): 更正command.py的文档信息

--- src/core/command.py
+++ src/core/command.py
@@ -16,8 +16,8 @@ def command(tokens: list, permission_level: PermissionLevel = PermissionLevel.US
         创建一条命令。
 
         :param tokens: 指令的调用名。
-        :param permission_level: 执行需要的权限等级，默认为0都可执行，1代表至少是mod，2代表至少是admin
-        :param is_command: 代表该条指令需不需要前置/
+        :param permission_level: 执行需要的权限等级，默认为USER，代表用户都可执行，MOD为内容审核用户，ADMIN为管理员
+        :param is_command: 代表该条指令需不需要前置 "/"
         :param need_check_exclude: 代表该条指令是否需要检查群号白名单
     """
 
