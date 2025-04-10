9f546e2ea07e4c454d867912ead58ab2850e3034
Floating-Ocean <sea113290980@qq.com>
2025-04-11 04:49:16 +0800

    test(Module, Render): 分离Render部分测试

---
+++ unittest/test_render.py
@@ -0,0 +1,47 @@
+import random
+import unittest
+
+from src.core.tools import png2jpg
+from src.module.color_rand import load_colors, _colors, transform_color, add_qrcode
+from src.platform.cp.atcoder import AtCoder
+from src.platform.cp.codeforces import Codeforces
+from src.platform.cp.nowcoder import NowCoder
+from src.render.render_color_card import ColorCardRenderer
+from src.render.render_contest_list import ContestListRenderer
+
+
+class Render(unittest.TestCase):
+
+    def test_color_rand(self):
+        load_colors()
+        picked_color = random.choice(_colors)
+        hex_raw_text, rgb_raw_text, hsv_raw_text = transform_color(picked_color)
+        color_card = ColorCardRenderer(picked_color, hex_raw_text, rgb_raw_text, hsv_raw_text).render()
+        color_card.write_file("test_color_rand.png")
+
+    def test_color_qrcode(self):
+        load_colors()
+        picked_color = random.choice(_colors)
+        hex_raw_text, rgb_raw_text, hsv_raw_text = transform_color(picked_color)
+        color_card = ColorCardRenderer(picked_color, hex_raw_text, rgb_raw_text, hsv_raw_text).render()
+        color_card.write_file("test_color_qrcode.png")
+        add_qrcode("test_color_qrcode.png", picked_color)
+        png2jpg("test_color_qrcode.png")
+
+    def test_contest_list(self):
+        upcoming_contests, running_contests, finished_contests = [], [], []
+        for platform in [Codeforces, AtCoder, NowCoder]:
+            upcoming, running, finished = platform.get_contest_list()
+            upcoming_contests.extend(upcoming)
+            running_contests.extend(running)
+            finished_contests.extend(finished)
+
+        upcoming_contests.sort(key=lambda c: c.start_time)
+        running_contests.sort(key=lambda c: c.start_time)
+        finished_contests.sort(key=lambda c: c.start_time)
+
+        contest_list_img = ContestListRenderer(upcoming_contests, running_contests, finished_contests).render()
+        contest_list_img.write_file("test_contest_list.png")
+
+if __name__ == '__main__':
+    unittest.main()
